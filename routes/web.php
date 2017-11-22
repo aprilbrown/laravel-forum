a090a20c0f88b874e93179c870f63c06b294e2d2
April Brown <april.brown@clearlink.com>
2017-11-22 14:57:25 -0700

    Lesson 6 - user can create threads

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/threads', 'ThreadsController@index');
+Route::post('/threads', 'ThreadsController@store');
 Route::post('threads/{thread}/replies', 'RepliesController@store');
 Route::get('/threads/{thread}', 'ThreadsController@show');
 
