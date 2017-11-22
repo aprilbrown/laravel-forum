534fea33f01edd3e871d8e907188650c99ad92f1
April Brown <april.brown@clearlink.com>
2017-11-22 14:28:34 -0700

    Lesson 5 - adding form for replies

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/threads', 'ThreadsController@index');
-Route::get('/threads/{thread}', 'ThreadsController@show');
 Route::post('threads/{thread}/replies', 'RepliesController@store');
+Route::get('/threads/{thread}', 'ThreadsController@show');
 
 
