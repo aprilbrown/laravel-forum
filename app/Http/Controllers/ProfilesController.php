cd9be1b53bf7725737e4ef258a4e939f5403ec28
April Brown <april.brown@clearlink.com>
2018-01-29 15:45:32 -0700

    Lesson 27 - finishing up activity feed.

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -9,10 +9,6 @@ class ProfilesController extends Controller
 {
     public function show(User $user)
     {
-        // $activities = $user->activity()->latest()->with('subject')->take(50)->get()->groupBy(function ($activity) {
-        //     return $activity->created_at->format('Y-m-d');
-        // });
-
         return view('profiles.show',[
             'profileUser' => $user,
             'activities' => Activity::feed($user)
