713f1fe47d373740c67d39223e0c9154a6aa0c75
April Brown <april.brown@clearlink.com>
2018-02-02 08:45:07 -0700

    Lesson 30 - adding favorite recording

--- app/Reply.php
+++ app/Reply.php
@@ -31,4 +31,9 @@ class Reply extends Model
     {
         return $this->belongsTo(Thread::class);
     }
+
+    public function path()
+    {
+        return $this->thread->path() . "#reply-{$this->id}";
+    }
 }
