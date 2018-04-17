40f0a3e6c9219245bb9f309f09bce3565112a242
April Brown <april.brown@clearlink.com>
2018-04-17 15:51:54 -0600

    Lesson 84
    Start of administrator being able to lock threads.

---
+++ tests/Feature/LockThreadsTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class LockThreadsTest extends TestCase
+{
+    use DatabaseMigrations;
+
+    /** @test */
+    public function an_administrator_can_lock_any_thread()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread');
+
+        $thread->lock();
+
+        $this->post($thread->path() . '/replies', [
+            'body' => "Test Thread",
+            'user_id' => auth()->id()
+        ])->assertStatus(422);
+    }
+}
