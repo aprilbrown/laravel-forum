94bc580160a58752a1ee839408530208b5665e10
April Brown <april.brown@clearlink.com>
2018-04-18 08:44:01 -0600

    Lesson 86
    Start of the front end for locking a thread.

--- resources/assets/js/pages/Thread.vue
+++ resources/assets/js/pages/Thread.vue
@@ -2,11 +2,12 @@
     import Replies from '../components/Replies.vue';
     import SubscribeButton from '../components/SubscribeButton.vue';
     export default {
-        props: ['initialRepliesCount'],
+        props: ['dataRepliesCount', 'dataLocked'],
         components: {Replies, SubscribeButton},
         data() {
             return {
-                repliesCount: this.initialRepliesCount
+                repliesCount: this.initialRepliesCount,
+                locked: this.dataLocked
             };
         }
     }
