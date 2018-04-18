e223714775329df1281a04f642ba9edfeb23a2d1
April Brown <april.brown@clearlink.com>
2018-04-18 13:40:39 -0600

    Reformatting
    Removing unneeded code and updating git ignore to ignore public files.

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -16,8 +16,6 @@ abstract class TestCase extends BaseTestCase
         parent::setUp();
 
         DB::statement('PRAGMA foreign_keys=on;');
-
-        $this->disableExceptionHandling();
     }
 
     protected function signIn($user = null)
@@ -25,26 +23,6 @@ abstract class TestCase extends BaseTestCase
         $user = $user ?: create('App\User');
 
         $this->actingAs($user);
-
-        return $this;
-    }
-
-    protected function disableExceptionHandling()
-    {
-        $this->oldExceptionHandler = $this->app->make(ExceptionHandler::class);
-
-        $this->app->instance(ExceptionHandler::class, new class extends Handler {
-            public function __construct() {}
-            public function report(\Exception $e) {}
-            public function render($request, \Exception $e) {
-                throw $e;
-            }
-        });
-    }
-
-    protected function withExceptionHandling()
-    {
-        $this->app->instance(ExceptionHandler::class, $this->oldExceptionHandler);
 
         return $this;
     }
