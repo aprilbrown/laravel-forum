abfb5ceec94d17433d8b97835ff363eee1e592d2
April Brown <april.brown@clearlink.com>
2019-10-03 17:21:01 -0600

    Upgrading to laravel 6

--- resources/views/threads/_list.blade.php
+++ resources/views/threads/_list.blade.php
@@ -20,7 +20,7 @@
                     </h5>
                 </div>
                 <a href="{{ $thread->path() }}">
-                    {{ $thread->replies_count }} {{ str_plural('reply', $thread->replies_count)}}
+                    {{ $thread->replies_count }} {{ \Illuminate\Support\Str::plural('reply', $thread->replies_count)}}
                 </a>
             </div>
         </div>
